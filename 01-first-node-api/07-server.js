093890e9e5b0312e4004a5eca64896fd0cf77c89
teksavyy <glitchfishzero@gmail.com>
2020-02-19 19:04:35 +0500

    feat: Real Time Chat on SSE (Server Sent Events) technology

---
+++ 01-first-node-api/07-server.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+const express = require('express');
+const EventEmitter = require('events');
+
+const chatEmitter = new EventEmitter();
+const port = process.env.PORT || 1337;
+
+const app = express();
+
+app.get('/', respondText);
+app.get('/json', respondJson);
+app.get('/echo', respondEcho);
+app.get('/static/*', respondStatic);
+app.get('/chat', respondChat);
+app.get('/sse', respondSSE);
+
+app.listen(port, () => console.log(`Server listening on port${port}`));
+
+function respondText(req, res) {
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('hi');
+}
+
+function respondJson(req, res) {
+  res.json({ text: 'hi', numbers: [1, 2, 3] });
+}
+
+function respondEcho(req, res) {
+  const { input = '' } = req.query;
+  res.json({
+    normal: input,
+    shouty: input.toUpperCase(),
+    characterCount: input.length,
+    backwards: input
+      .split('')
+      .reverse()
+      .join('')
+  });
+}
+
+function respondStatic(req, res) {
+  const filename = `${__dirname}/public/${req.params[0]}`;
+  fs.createReadStream(filename)
+    .on('error', () => respondNotFound(req, res))
+    .pipe(res);
+}
+
+function respondChat(req, res) {
+  const { message } = req.query;
+  chatEmitter.emit('message', message);
+  res.end();
+}
+
+function respondSSE(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    Connection: 'keep-alive'
+  });
+
+  const onMessage = msg => res.write(`data: ${msg}\n\n`);
+  chatEmitter.on('message', onMessage);
+  res.on('close', function() {
+    chatEmitter.off('message', onMessage);
+  });
+}
+
+function respondNotFound(req, res) {
+  res.writeHead(404, { 'Content-Type': 'text/plain' });
+  res.end('Not Found');
+}
